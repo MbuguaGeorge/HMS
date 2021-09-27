2741d0c03ba9dda481e2b3e42cef6e04d23da312
titusowuor30 <titusowuor30@gmail.com>
2021-09-27 14:39:39 +0300

    configuring heroku settings

--- HMS/urls.py
+++ HMS/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls import url
+from django.views.static import serve
 from django.conf import settings
 from django.contrib.staticfiles.urls import static, staticfiles_urlpatterns
 
